9f678770ed715b3e09a6d0503a0c0806fea9a3ec
juampa <49341476+juampacamarda@users.noreply.github.com>
2022-06-26 21:09:09 -0300

    updates varias
    Empece a revisar el functions

--- app/public/wp-content/themes/remo-theme/functions.php
+++ app/public/wp-content/themes/remo-theme/functions.php
@@ -9,59 +9,6 @@ function custom_url_login() {
 }
 add_filter( 'login_headerurl', 'custom_url_login' );
 
-//custom logo
-function mytheme_setup() {
-    add_theme_support('custom-logo', array(
-	'flex-height' => true,
-	'flex-width'  => true,
-	'header-text' => array( 'site-title', 'site-description' ),
-) );}
-
- 
-add_action('after_setup_theme', 'mytheme_setup');
-//cambiar la clase del logo
-add_filter( 'get_custom_logo', 'change_logo_class' );
-
-function change_logo_class( $html ) {
-
-    $html = str_replace( 'custom-logo-link', 'home-link-lg d-none d-lg-block', $html );
-	$html = str_replace( 'custom-logo', 'img-fluid',  $html );
-
-    return $html;
-}
-add_filter( 'get_custom_logo', function( $html, $blog_id ) {
-    $html = str_replace( 'itemprop="logo"', 'itemprop="logo" title="Vita Salud Empresa de Servicios de Ortopedia y Rehabilitación
-    Alquiler y Venta de Artículos Ortopédicos"', $html );
-    return $html;
- }, 10, 2 );
-//nav menu and thumbnails
-if (function_exists('register_nav_menus')){
-	register_nav_menus (array('superior'=>'Menu Principal Superior'));
-	register_nav_menus (array('wootienda'=>'menu tienda'));
-	register_nav_menus (array('footer'=>'Menu footer b'));
-
-}
-function add_link_atts($atts) {
-$atts['class'] = "nav-link";
-return $atts;
-}
-add_filter( 'nav_menu_link_attributes', 'add_link_atts');
-//navlink
-if ( function_exists( 'add_theme_support' ) ) { 
-		add_theme_support( 'post-thumbnails' );
-}
-//mostrar barra admin
-if (is_user_logged_in()) {
-    show_admin_bar(true);
-}#end if
-//abajo carga shortcodes
-// Adding Shortcodes to the_excerpt() function
-add_filter('the_excerpt', 'do_shortcode');
-add_filter('acf/format_value/type=textarea', 'do_shortcode');
-add_filter('acf/format_value/type=text', 'do_shortcode');
-add_filter('acf/format_value', 'do_shortcode');
-// Enable shortcodes in widgets
-add_filter('widget_text', 'do_shortcode');
 //abajo carga estilos
 function theme_scripts(){
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() .'/assets/css/base-style.css');
@@ -75,12 +22,73 @@ function theme_scripts(){
     wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' );
     wp_enqueue_script( 'bootstrapjs', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' );
     wp_enqueue_script( 'prefix-font-awesome', 'https://kit.fontawesome.com/ef7428ba99.js' );
-    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() .'/assets/js/theme-consultar.js');
+    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() .'/assets/js/theme-Remo.js');
     wp_enqueue_script('owljs', get_stylesheet_directory_uri().'/assets/owlcarousel/owl.carousel.min.js');
     wp_enqueue_script('aosjs', 'https://unpkg.com/aos@next/dist/aos.js');
 
 }
 add_action('wp_enqueue_scripts','theme_scripts');
+
+
+//agregar custom logo
+function mytheme_setup() {
+    add_theme_support('custom-logo', array(
+	'flex-height' => true,
+	'flex-width'  => true,
+	'header-text' => array( 'site-title', 'site-description' ),
+) );}
+add_action('after_setup_theme', 'mytheme_setup');
+
+//cambiar la clase del logo
+add_filter( 'get_custom_logo', 'change_logo_class' );
+
+function change_logo_class( $html ) {
+
+    $html = str_replace( 'custom-logo-link', 'home-link-lg d-none d-lg-block', $html );
+	$html = str_replace( 'custom-logo', 'img-fluid',  $html );
+
+    return $html;
+}
+add_filter( 'get_custom_logo', function( $html, $blog_id ) {
+    $html = str_replace( 'itemprop="logo"', 'itemprop="logo" title=""', $html );
+    return $html;
+ }, 10, 2 );
+
+//nav menu and thumbnails
+
+if (function_exists('register_nav_menus')){
+	register_nav_menus (array('superior'=>'Menu Principal Superior'));
+	register_nav_menus (array('wootienda'=>'menu tienda'));
+	register_nav_menus (array('footer'=>'Menu footer b'));
+
+}
+function add_link_atts($atts) {
+$atts['class'] = "nav-link";
+return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_link_atts');
+//navlink
+
+
+if ( function_exists( 'add_theme_support' ) ) { 
+		add_theme_support( 'post-thumbnails' );
+}
+
+//mostrar barra admin
+if (is_user_logged_in()) {
+    show_admin_bar(true);
+}#end if
+
+//abajo carga shortcodes
+add_filter('the_excerpt', 'do_shortcode');
+add_filter('acf/format_value/type=textarea', 'do_shortcode');
+add_filter('acf/format_value/type=text', 'do_shortcode');
+add_filter('acf/format_value', 'do_shortcode');
+
+// Enable shortcodes in widgets
+add_filter('widget_text', 'do_shortcode');
+
+
 //subir svg
 function custom_mimes( $mimes = array() ) {
 	// New allowed mime types.
@@ -90,6 +98,7 @@ function custom_mimes( $mimes = array() ) {
 }
 add_filter( 'upload_mimes', 'custom_mimes' );
 //fin subir svg
+
 //woocomerce
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
@@ -97,11 +106,11 @@ add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
 function my_theme_wrapper_start() {
-    echo '<section id="main"><div class="container py-3">';
+    echo '<main class="site-content"><div class="container py-3">';
 }
 
 function my_theme_wrapper_end() {
-    echo '</div></section>';
+    echo '</div></main>';
 }
 
 function plantilla_add_woocommerce_support() {
@@ -125,7 +134,7 @@ function plantilla_add_woocommerce_support() {
 }
 add_action( 'after_setup_theme', 'plantilla_add_woocommerce_support');
 
-function dcms_agregar_nueva_zona_widgets() {
+function zona_widgets_woo() {
 
     register_sidebar( array(
         'name'          => 'widget-woocommerce',
@@ -138,7 +147,7 @@ function dcms_agregar_nueva_zona_widgets() {
     ) );
     
     }
-add_action( 'widgets_init', 'dcms_agregar_nueva_zona_widgets' );
+add_action( 'widgets_init', 'zona_widgets_woo' );
 
 function relacionados(){
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
@@ -147,10 +156,52 @@ function relacionados(){
  add_action( 'woocommerce_before_main_content', 'relacionados' );
 
  /* ACF */
+ 
+ add_filter('acf/settings/save_json', 'remo_acf_json_save_point');
+ 
+ function remo_acf_json_save_point( $path ) {
+     
+     // update path
+     $path = get_stylesheet_directory() . '/acf-json';
+     
+     // return
+     return $path;
+     
+ }
+
+ add_filter('acf/settings/load_json', 'remo_acf_json_load_point');
+
+ function remo_acf_json_load_point( $paths ) {
+     
+     // remove original path (optional)
+     unset($paths[0]);
+     
+     // append path
+     $paths[] = get_stylesheet_directory() . '/acf-json';
+     
+     // return
+     return $paths;
+     
+ }
+
+
  if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page();
-	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Opciones Remo theme',
+		'menu_title'	=> 'Remo theme',
+		'menu_slug' 	=> 'Remo-theme',
+		'redirect'		=> false,
+		'position'		=> 5.4
+	));
+
+	/*
+    acf_add_options_sub_page(array(
+		'page_title' 	=> 'Slide Home',
+		'menu_title'	=> 'slide',
+		'parent_slug'	=> 'Remo-theme',
+	));
+    */
+
 }
 
 
@@ -168,17 +219,17 @@ function widget_buscador() {
     }
 	add_action( 'widgets_init', 'widget_buscador' );
 	
-/**
+/*
  * Register Custom Navigation Walker
  */
 function register_navwalker(){
 	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
+
+
 //agregar iconito carro
-/**
- * Ensure cart contents update when products are added to the cart via AJAX
- */
+
 function my_header_add_to_cart_fragment( $fragments ) {
  
     ob_start();
@@ -203,9 +254,10 @@ add_action( 'woocommerce_before_shop_loop_item_title', function() {
        echo '<span class="now_sold">AGOTADO</span>';
    }
 });
+
 /*solo productos en la busqueda*/
-//Only show products in the front-end search results
-function lw_search_filter_pages($query) {
+
+function remo_search_filter_pages($query) {
     if ($query->is_search) {
         $query->set('post_type', 'product');
         $query->set( 'wc_query', 'product_query' );
@@ -213,22 +265,29 @@ function lw_search_filter_pages($query) {
     return $query;
 }
  
-add_filter('pre_get_posts','lw_search_filter_pages');
+add_filter('pre_get_posts','remo_search_filter_pages');
+
 /* funcion formulario de productos */
 function formulario_productos(){
     echo get_template_part( './contactoproductos' );
 }
+
 /*function aviso_tienda(){
     echo get_template_part( './woocommerce/aviso-tienda' );
 }*/
+
 /* formulario de productos */
 /* agregar formulario de productos */
 add_action( 'woocommerce_after_single_product_summary', 'formulario_productos', 16 );
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
-remove_action ( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+
+//remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
+
+//remove_action ( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+
 //add_action( 'woocommerce_before_cart_totals', 'aviso_tienda' );
 
-/*add_filter('woocommerce_states', 'lista_provincias');
+/*
+add_filter('woocommerce_states', 'lista_provincias');
 
 function lista_provincias( $provincias ) {
 
@@ -240,7 +299,9 @@ $provincias ['AR'] = array(
 
 return $provincias;
 
-}*/
+}
+*/
+
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
 
 /**
@@ -255,7 +316,7 @@ if (!function_exists('loop_columns')) {
 
  function loop_columns() {
 
- return 3; // 3 productos por columna
+ return 4; //4 productos por columna
 
  }
 
