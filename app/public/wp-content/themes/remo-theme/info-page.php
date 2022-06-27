b051e225be5d472132be5de93b44c59d320bdc53
juampa <49341476+juampacamarda@users.noreply.github.com>
2022-06-26 23:10:01 -0300

    ultimos ajustes para setear la base

--- app/public/wp-content/themes/remo-theme/info-page.php
+++ app/public/wp-content/themes/remo-theme/info-page.php
@@ -7,58 +7,105 @@ Template Name: page-plantilla
 <main class="site-content">
    
         <?php if ( get_field( 'imagen_portada') ) { ?>
-            <div class="heading d-flex align-items-center" style="background-position: bottom;background-image:url('<?php the_field( 'imagen_portada' ); ?>'); background-size:cover; background-repeat:no-repeat">
+            <div class="heading d-flex align-items-center" style="background-image:url('<?php the_field( 'imagen_portada' ); ?>'); background-size:cover; background-repeat:no-repeat">
                 <div class="container">
-                    <h2 class="tittle cursiva" data-aos="fade-up" > <?php the_field( 'titulo' ); ?> </h2>
+                    <h4 class="pre-txt"><?php the_field( 'pre_txt' ); ?></h4>
+                    <h2 class="tittle cursiva"> <?php the_title(); ?> </h2>
+                    <p class="header_bajada"><?php the_field( 'header_bajada' ); ?></p>
                 </div>
             </div>
-        <?php } ?>
-    <div class="contenido py-4">
-
+        <?php } ?>        
     
-    <?php $imagen_contenido = get_field( 'imagen-contenido' ); ?>
-    <?php if ( $imagen_contenido ) : ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 p-5" data-aos="fade-up">
-                    <img src="<?php echo esc_url( $imagen_contenido['url'] ); ?>" alt="<?php echo esc_attr( $imagen_contenido['alt'] ); ?>" class="img-fluid d-block" />
-                </div>
-                <div class="col-md-7">
-                    <div class="content-page" data-aos="fade-up">
-                        <div class="texto-post d-flex flex-column">
-                            <?php if( get_field( 'titulo_txt' )) { ?>
-                                <h4 class="mb-4"><?php the_field( 'titulo_txt' ); ?></h4>
-                            <?php } ?>
-                            <div class="cuerpo mb-4">
-                                <?php the_field( 'texto_contenido' ); ?>
-                            </div>
-                            <?php $button = get_field( 'button' ); ?>
-                            <?php if ( $button ) : ?>
-                                <a class="btn btn-contacto mt-auto ml-auto" href="<?php echo esc_url( $button) ; ?>">Contactanos</a>
-                            <?php endif; ?>
-                        </div>
-                        
+        <div class="content-page d-flex flex-column">
+
+            <?php if(get_field('texto_contenido') ){ ?>
+                <div class="texto-post textppal" style="order:<?php the_field( 'orden_de_bloque_texto' ); ?>;">
+                    <div class="container">
+                        <?php the_field( 'texto_contenido' ); ?>
                     </div>
                 </div>
-            </div>
+            <?php } ?>
+
+            <?php if( have_rows('collapses') ): ?>
+            
+                <div class="colapsable texto-post" style="order:<?php the_field( 'orden_de_bloque_collapse' ); ?>;">
+                    <div class="container">
+                        <?php while( have_rows('collapses') ): the_row(); ?>
+                            <h3 class="d-flex justify-content-between">
+                                <a class="collapse-link" data-toggle="collapse" href="#pregunta-<?php the_sub_field('numero'); ?>" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                    <?php the_sub_field('pregunta'); ?>
+                                </a>
+                                <i class="fa fa-angle-down"></i>
+                            </h3>
+                            <div class="collapse" id="pregunta-<?php the_sub_field('numero'); ?>">
+                            <div class="card card-body">
+                                <?php the_sub_field('respuesta'); ?>
+                            </div>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+          
+            <?php endif; ?>
+
+            <?php if( have_rows('clientes_logos') ): ?>
+                <div class="lista_logos" style="order:<?php the_field( 'orden_de_bloque_logo' ); ?>;">
+                    <div class="container">
+                        <ul class="Logos productos nav nav-bar">
+
+                                <?php while( have_rows('clientes_logos') ): the_row(); 
+                                // vars
+                                $imagen = get_sub_field('imagenes');
+                                ?>
+                                    <li class="nav-item">
+                                        <?php if( the_sub_field('imagenes') ){ ;?>
+                                            <img src="<?php the_sub_field('imagenes') ?>" alt="" class="d-block img-fluid">
+                                        <?php } ;?>
+                                        <?php if( the_sub_field('info') ){ ;?>
+                                            <p><?php the_sub_field('info') ?></p>
+                                        <?php } ;?>
+                                    </li> 
+                                    
+                                <?php endwhile; ?>
+                            
+                        </ul>
+                    </div>
+                </div>
+            <?php endif; ?>
+            
+			<?php if(get_field('codigo_form') ){ ?>
+                <div  class="form texto-post" 
+											style=" <?php if(get_field('orden_form') ){ ?>
+											order:<?php the_field( 'orden_form' ); ?><?php } ?>;
+											 <?php if ( get_field( 'fondo_form' ) ) { ?> 
+											 background-image:url(<?php the_field( 'fondo_form' ); ?>);<?php }?>" >
+                    <div class="container">
+											<?php the_field( 'codigo_form' ); ?>
+                    </div>
+                </div>
+            <?php } ?>
+
+			<?php if(get_field('banner-titulo') ){ ?>
+                <div  class="banner texto-post" 
+											style=" <?php if(get_field('orden_banner') ){ ?>
+											order:<?php the_field( 'orden_banner' ); ?><?php } ?>;
+											 <?php if ( get_field( 'imagen_banner' ) ) { ?> 
+											 background-image:url(<?php the_field( 'imagen_banner' ); ?>);<?php }?>" >
+                    <div class="container">
+											<h3><?php the_field( 'banner-titulo' ); ?></h3>
+											<h5 class="pretxt"><?php the_field( 'banner-pretxt' ); ?></h5>
+											<?php $link = get_field( 'link' ); ?>
+											<?php if ( $link ) : ?>
+											<a href="<?php the_field( 'link' ); ?>" target="blank" class="btn btn-landing">
+												CONTACTANOS
+											</a>
+											<?php endif; ?>
+
+                    </div>
+                </div>
+            <?php } ?>
+
+
         </div>
-    <?php else: ?>
-        <div class="container">
-            <div class="content-page  d-flex flex-column">
-                <?php if( get_field( 'titulo_txt' )) { ?>
-                <h4><?php the_field( 'titulo_txt' ); ?></h4>
-                <?php } ?>
-                <div class="texto-post"><?php the_field( 'texto_contenido' ); ?></div>
-                <?php $button = get_field( 'button' ); ?>
-                <?php if ( $button ) : ?>
-                    <a class="btn btn-contacto mt-auto ml-auto" href="<?php echo esc_url( $button) ; ?>">Contactanos -></a>
-                <?php endif; ?>
-            </div>
-        </div>
-    <?php endif; ?>
-    </div>
-    
-    
-    
 </main> <!--fin main-->
 <?php get_footer();?> 
